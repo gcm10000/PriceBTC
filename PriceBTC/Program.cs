71728fcb58afae6bb19ac57c5a5c2f8f482fb257
gcm10000 <gabrielc.machado@hotmail.com>
2021-08-23 14:18:37 -0300

    Correção de cálculo.

--- PriceBTC/Program.cs
+++ PriceBTC/Program.cs
@@ -1,32 +1,40 @@
 ﻿using System;
+using System.Globalization;
 
 namespace PriceBTC
 {
     class Program
     {
         //Comissão de compra e venda do Mercado Bitcoin
-        static float Comission = 0.01f;
+        static double Comission = 0.01f;
         static void Main(string[] args)
         {
             Console.Write("Antigo preço: ");
-            float oldPrice = float.Parse(Console.ReadLine());
+            double oldPrice = double.Parse(Console.ReadLine());
             Console.Write("Preço atual: ");
-            float newPrice = float.Parse(Console.ReadLine());
-            float pp = 1;
-            float percentageProcessor = 0.00f;
+            double newPrice = double.Parse(Console.ReadLine());
+            double pp = 1;
+            double percentageProcessor = 0.00f;
             if (newPrice < oldPrice)
             {
                 pp = (newPrice / oldPrice);
-                percentageProcessor = (1 - pp) * (-1);
+                percentageProcessor = (1 / pp) - 1;
             }
             else if (oldPrice < newPrice)
             {
                 pp = (oldPrice / newPrice);
-                percentageProcessor = 1 - pp;
+                percentageProcessor = pp - 1;
             }
 
-            Console.WriteLine(Environment.NewLine + $"Rentabilidade bruta: {(percentageProcessor * 100)}");
-            Console.WriteLine(Environment.NewLine + $"Rentabilidade líquida: {(percentageProcessor - Comission) * 100 }");
+
+            Console.WriteLine(Environment.NewLine + $"Rentabilidade bruta: {(percentageProcessor.ToString("P", new CultureInfo("pt-BR", false).NumberFormat))}");
+            Console.WriteLine(Environment.NewLine + $"Rentabilidade líquida: {(percentageProcessor - Comission).ToString("P", new CultureInfo("pt-BR", false).NumberFormat)}");
+            Pause();
+        }
+        static void Pause()
+        {
+            Console.Write("Pressione uma tecla para continuar. . .");
+            Console.ReadKey();
         }
     }
 }
